bd19cc568eceb371ae720b97689f44181bf11149
s-ijiogbe <s.ijiogbe@gmail.com>
2025-01-28 01:26:26 -0500

    Included and worked on the templates for my project base

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
-}
+    path('room/<str:pk>/', views.room, name="room"),
+]
 
 
