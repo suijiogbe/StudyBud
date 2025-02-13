1b3324f930d59b2c2b7c3b915ee1189b3ec682e2
Stephanie Ijiogbe <sijiogbe@charlotte.edu>
2025-02-12 21:15:35 -0500

    Worked on the logout function

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from.forms import RoomForm
 
@@ -24,8 +25,21 @@ def loginPage(request):
         except:
             messages.error(request, 'User does not exist')
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
